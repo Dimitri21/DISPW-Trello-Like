34ea87b878c5debb3d12d2285e25219721406dd9
KALUMVUATI Duramana <duramana.kalumvuati@laposte.net>
2021-01-14 22:23:57 +0100

    [NAV_HEADER] mise en place la structure de la barre nav et Header

--- app/Controller/HomeController.php
+++ app/Controller/HomeController.php
@@ -1,28 +1,26 @@
 <?php
 
 namespace App\Controller;
+
 use Core\Controller\Controller;
 use \App;
 
-
 class  HomeController extends  AppController
 {
-    private $db_name ='';
+    private $db_name = '';
 
     public function  __construct()
     {
         parent::__construct();
         //Load elements to show on the home page
-       // $this->loadModel('Tutoriel','tutofree_openclassroom');
+        // $this->loadModel('Tutoriel','tutofree_openclassroom');
         $this->db_name = App::getInstance()->db_name;
     }
 
     public function  index()
     {
-        $this->render('home.index',compact(''));
+        $variable = "rien du tout";
+        $this->render('home.index', compact('variable'));
         //$this->render('modules.index',compact('modules','banners','math_et_matik','tutoriels','portofolios'));
     }
-
 }
-
-?>
