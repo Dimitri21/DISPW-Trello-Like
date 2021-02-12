72c88defda7b9c0d324ea804ce539e9e442ba8f4
Dimitri21 <71702427+Dimitri21@users.noreply.github.com>
2021-02-12 20:57:44 +0100

    politique de confidentialite

--- app/Views/home/confidentialite.php
+++ app/Views/home/confidentialite.php
@@ -1,12 +1,217 @@
 
 
-<div class="row" id="cgu-index">
+<section class="confidentialite" id="cgu-index">
 
-    <div class="">
-        <h2 id="info"> Confidentialités </h2>
-    </div>
-
+<div class="">
+    <h1> Politique de confidentialité </h1>
 </div>
+
+<article>
+    <h2>GLOSSAIRE</h2>
+
+    <p>GLOSSAIRE Chaque terme débutant par une majuscule a le sens qui lui est donné ci-après.</p>
+
+    <table>
+        <tbody>
+            <tr>
+                <td>« Destinataire »:</td>
+                <td>désigne le service ou l’entreprise qui reçoit communication et peut accéder à vos Données à Caractère Personnel.</td>
+            </tr>
+            <tr>
+                <td>« Données à Caractère Personnel »:</td>
+                <td>désigne toute information se rapportant à vous et permettant de vous identifier directement ou indirectement.</td>
+            </tr>
+            <tr>
+                <td>« Politique de confidentialité et de protection des Données à Caractère Personnel » et « Politique »</td>
+                <td>désigne la présente Politique décrivant les mesures prises pour le traitement, l’exploitation et la gestion de vos Données à Caractère Personnel et vos droits en tant que personne concernée par le traitement.</td>
+            </tr>
+            <tr>
+                <td>« Responsable du traitement »:</td>
+                <td>désigne une personne de l’entreprise la Société qui réalise le traitement de vos Données à Caractère Personnel.</td>
+            </tr>
+            <tr>
+                <td>«Société»:</td>
+                <td>désigne la société fictive TrelloLike.webo</td>
+            </tr>
+            <tr>
+                <td>« Traitement »:</td>
+                <td>désigne toute opération ou tout ensemble d'opérations appliquées à vos Données à Caractère Personnel.</td>
+            </tr>
+            <tr>
+                <td>« Violation de données à caractère personnel »:</td>
+                <td>désigne une violation de la sécurité, entraînant, de manière accidentelle ou illicite, la destruction, la perte, l'altération, la divulgation ou l’accès non autorisés à vos Données à Caractère Personnel.</td>
+            </tr>
+        </tbody>
+    </table>
+</article>
+
+<article>
+
+    <p>Ce  document  vous  explique  les  principes  et  nos  engagements  pour  la  protection  de  vos Données à Caractère Personnel et a pour objectif de vous informer sur : </p>
+
+    <ul>
+        <li>les  Données  à  Caractère  Personnel  que  la  Société  collecte  et  les  raisons  de  cette collecte,</li>
+        <li>la façon dont seront utilisées ces Données à Caractère Personnel,</li>
+        <li>Vos droits en qualité de personne concernée par nos traitements de données. Cette Politique s’applique à tous les services de la Société quel que soient leur nature (site, applications, prestations ...)</li>
+        <li>Le  Responsable  du  Traitement  de  vos  données Le  responsable  du  traitement  des données est ///// ////////.</li>
+            
+        <li>Les coordonnées de contact sontles suivantes :</li>
+            <ul>
+                <li>Adresse postale :</li>
+                <li>Téléphone :</li>
+                <li>Courrier électronique : contact@trello.webo</li>
+            </ul>
+
+    </ul>
+
+</article>
+
+<article>
+    <h2>Comment la Société met en œuvre la protection des Données à Caractère Personnel?</h2>
+
+    <p>La Société s’engage à prendre en compte la protection de vos Données à Caractère Personnel et de votre vie privée dès la conception des services qui vous sont proposés (Privacy by desing).</p>
+    <p>Pour  assurer  la  sécurité  et  garantir  le  respect  et  le  bon  exercice de  vos  droits,  les  mesures permettant d’assurer la protection de vos Données à Caractère Personnel sont mises en œuvre (Privacy by default).</p>
+
+</article>
+
+<article>
+    <h2>Quelles  Données  à  Caractère  Personnel  sont  utilisées  par  la Société ?</h2>
+
+    <p>La Société s’engage à ne collecter quedes données strictement nécessaires à la réalisation des services proposé.Dans le cas où des données facultatives vous seraient demandées, la Société vous informera clairement sur les Données à Caractère Personnel nécessaires à la réalisation du service.</p>
+    <p>Les  Données  à  Caractère  Personnel  sont  collectées  directement  auprès  de  vous  et  ne  sont utilisées que pour les usages qui ont été portés à votre connaissance.Les Données à Caractère Personnel sont utilisées pour vous proposer d’autres services, uniquement si vous avez accepté de recevoir des communications commerciales.</p>
+
+</article>
+
+<article>
+    <h2>Quel est le fondement de la légitimité de nos traitements ?</h2>
+
+    <p>La  Société  s’appuie  sur  les  bases  légitimes  suivantes  afin  de  pouvoir  traiter  les  Données  à Caractère Personnel : </p>
+
+    <table>
+        <tbody>
+            <tr>
+                <td>Exécution du contrat</td>
+                <td>La base légale du traitement des Données à Caractère Personnel de  l’utilisateur  qui  sont  collectées  pour l'inscription sur la plateforme est  l’exécution  du  contrat  de vente ou  de  prestations  de  service.  À  cet  égard,  l’utilisateur  est obligé de fournir les données nécessaires à son exécution. S’il ne fournit   pas   ces   données,   éventuellement   au   prestataire   de paiement en ligne, il ne sera pas possible de réaliser la vente.</td>
+            </tr>
+        </tbody>
+    </table>
+
+</article>
+
+<article>
+    <h2>Les Données à Caractère Personnel des mineurs ? </h2>
+
+    <p>Certains services peuvent être utilisés par des mineurs. Dans ce cas, les mineurs doivent obtenir le consentement de leurs parents ou représentants légaux. </p>
+
+</article>
+
+<article>
+    <h2>A  qui  peuvent  être  communiquées  vos  Données  à  Caractère Personnel ?</h2>
+
+    <p>Vos données sont susceptibles d’être transmises :</p>
+
+    <ul>
+        <li>A des services internes de la Société : les directions qui sont en charge de l’exécution des services souscrits, notamment Service Clients, Administration des Ventes...</li>
+        <li>A des prestataires externes à la Société : l'herbergeur, les prestataires techniques, y compris des sous-traitants ; </li>
+        
+    </ul>
+</article>
+
+<article>
+    <h2>Vos    Données    à    Caractère    Personnel    peuvent-elles    être transférées en dehors de l’Union Européenne ?</h2>
+
+    <p>La  Société  réalise  l’ensemble  des  traitements  de  vos  Données  à  Caractère  Personnel  sur  le territoire  de  l’Union  Européenne  (UE). Toutefois,  pour  certaines  prestations  spécifiques,  la Société peut avoir recours à des sous-traitants établis en dehors de l’UE. Certaines Données à Caractère  Personnel  peuvent  alors  leur  être  communiquées  pour  les  stricts  besoins  de  leurs missions.</p>
+    <p>Dans  ce  cas,  conformément  à  la  règlementation  en  vigueur,  la  Société  exige  de  ses  sous-traitants qu’ils fournissent les garanties nécessaires à l’encadrement et à la sécurisation de ces transferts,  notamment  par  la  signature  de  clauses  contractuelles  types  de  la  Commission européenne.</p>
+</article>
+
+<article>
+    <h2>Pendant  combien  de  temps  la  Société  conserve  vos  Données  à Caractère Personnel?</h2>
+
+    <p>La durée de conservation de vos Données à Caractère Personnel dépend du service souscrit. la Société s’engage à ne pas conserver vos Données à Caractère Personnel au-delà de la durée nécessaire  à  la  fourniture  du  service,  et  donc  à  votre  utilisation  du  service,  augmentée  de  la durée de conservation imposée par les règles applicables en matière de prescription légale.</p>
+</article>
+
+<article>
+    <h2>Vos Données à Caractère Personnel sont-elles protégées ?</h2>
+    <p>La Société s’engage à prendre toutes mesures afin d’assurer la sécurité et la confidentialité des Données à Caractère Personnel et notamment à empêcher qu’elles ne soient endommagées, effacées ou que des tiers non autorisés y aient accès.Par ailleurs, en cas d’incident de sécurité affectant vos Données à Caractère Personnel (destruction, perte, altération ou divulgation), la Société s’engage à respecter l’obligation de notification des violations de Données à Caractère Personnel, notamment auprès de la CNIL.</p>
+</article>
+
+<article>
+    <h2>Quels sont vos droits sur vos Données à Caractère Personnel ?</h2>
+
+    <p>Vous disposez à tout moment de la faculté d’exercer auprès d’la Société les droits prévus par la  réglementation  en  vigueur  applicable  en  matière  de  données  à  caractère  personnel,  sous réserve d’en remplir les conditions :</p>
+
+    <table>
+        <tbody>
+            <tr>
+                <td>Droit d’accès</td>
+                <td>vous   pouvezavoir   communication   de   vos   Données   à   Caractère Personnel faisant l’objet d’un traitement par la Société.</td>
+            </tr>
+            <tr>
+                <td>Droit de rectification</td>
+                <td>vous pouvez mettre à jour vos Données à Caractère Personnel ou faire rectifier vos Données à Caractère Personnel traitées par la Société</td>
+            </tr>
+            <tr>
+                <td>Droit d’opposition <span class="mini">notamment à recevoir des communications commerciales</span></td>
+                <td>vous   pouvez   exprimer   votre   souhait   de   ne   plus   recevoir   de communication  commerciale  de  la  part  de  la  Société  ou  demander que  vos  Données  à  Caractère  Personnel  ne  fassent plus  l’objet  d’un traitement</td>
+            </tr>
+            <tr>
+                <td>Droit à l’effacement</td>
+                <td>vous  pouvez  demander  la  suppression  de  vos  Données  à  Caractère Personne.</td>
+            </tr>
+            <tr>
+                <td>Droit à la limitation</td>
+                <td>vous pouvez demander la suspension du traitement de vos Données à Caractère Personnel</td>
+            </tr>
+            <tr>
+                <td>Droit à la portabilité</td>
+                <td>vous  pouvez  demander  à  la  Société  de  récupérer  vos  Données  à Caractère Personnel afin d’en disposer. Lors de la souscription d’un service  ou  du  recueil  de  vos  Données  à  Caractère  Personnel,  il  vous est  indiqué  l’adresse  (postale  et/ou  électronique)  à  laquelle  envoyer votre  demande  d’exercice  de  vos  droits.  Toute  demande  doit  être accompagnée   d’un   justificatif   d’identité.   la   Société   s’engage   à répondre à vos demandes d’exercice de vos droits dans les meilleurs délais et en tout état de cause dans le respect des délais légaux.</td>
+            </tr>
+        </tbody>
+    </table>
+
+</article>
+
+<article>
+
+<h2>Modification de la présente politique </h2>
+
+<p>Modification de la présente politique Les mises à jour de la présente Politique en matière de confidentialité seront publiées sur le présent site.</p>
+</article>
+
+<article>
+    <h2>Politique en matière de cookies </h2>
+
+    <p>Les sites de la Société et toutes ses pages Web utilisent des cookies pour vous distinguer des autres  utilisateurs  du  site.  Cela  nous  permet  d'améliorer  les  services  et  d'optimiser  votre expérience  utilisateur.Un  cookie  consiste  en  une  chaîne  d'informations  au  format  texte transférée  par  un  site  Web  vers  le  disque  dur  de  votre  ordinateur  afin  que  le  site  Web  se souvienne  de  vous.  Les  cookies  peuvent  aider  un  site  Web  à  adapter  plus  rapidement  le contenu à vos centres d'intérêt. La plupart des sites Web importants utilisent des cookies. En général,  un  cookie  contient  le  nom  du  domaine  d'origine  du  cookie  ;  la  "durée  de  vie"  du cookie et une valeur, à savoir un numéro unique créé de façon aléatoire.</p>
+
+    <p>Sur les sites de la Société, il existe deux types de cookies :</p>
+
+    <ul>
+        <li>des  cookies  temporaires,  qui  demeurent  dans  le  fichier  cookie  de  votre  navigateur jusqu'à ce que vous quittiez le site</li>
+        <li>des cookies persistants, qui demeurent beaucoup plus longtemps dans le fichier cookie de votre navigateur (la durée dépend de la durée de vie du cookie en question).</li>
+    </ul>
+
+    <p>Les cookies temporaires sont utilisés :</p>
+
+    <ul>
+        <li>pour vous permettre de transférer des informations d'une page à l'autre de notre site sansavoir à les saisir à nouveau</li>
+        <li>pour vous permettre d'accéder aux informations d'enregistrement conservées.</li>
+    </ul>
+
+    <p>Les cookies persistants sont utilisés : </p>
+
+    <ul>
+        <li>pour  nous  aider  à  vous  identifier  en  tant  que  visiteur  (à  l'aide  d'un  numéro,  pas personnellement) lorsque vous revenez sur notre site Web</li>
+        <li>Pour compiler des statistiques globales anonymes qui nous permettent de comprendre comment les visiteurs utilisent notre site et nous aident à améliorer la structure de notre site Web. Nous ne pouvons pas vous identifier personnellement de cette façon. </li>
+    </ul>
+
+    <p>Vous avez la possibilité d'accepter ou de refuser les cookies en modifiant les paramètres de votre navigateur. Cependant, vous risquez de perdre certaines fonctions interactives du service si les cookies sont désactivés.</p>
+
+    <p>Vous pouvez supprimer et bloquer tous les cookies en provenance des sites de la Société. Pour ce faire, modifiez le paramètre de votre navigateur permettant de refuser certains ou tous les cookies.  Sachez  cependant  que  si  vous  décidez  de  bloquer  les  cookies  via  votre  navigateur, certains éléments de notre site Web risquent de ne pas fonctionner correctement. </p>
+
+</article>
+
+
+</section>
 
 
 
