14aeb7fe566c60d2cfea2682f96df0a98065d060
KALUMVUATI Duramana <duramana.kalumvuati@laposte.net>
2021-02-21 11:07:13 +0100

    [REFACTORING]

--- app/sprinto.php
+++ app/sprinto.php
@@ -2,11 +2,7 @@
 
 return  array(
      "db_name" => "sprinto",
-     "db_user" => "sprinto",
-     "db_pass" => "5pr1nt0CC1",
-     "db_host" => "localhost",
-//    "db_name" => "dbs1457140",
-//    "db_user" => "dbu1541643",
-//    "db_pass" => "Sprint@Trell@Like2021",
-//    "db_host" => "db5001765458.hosting-data.io",
+     "db_user" => "root",
+     "db_pass" => "",
+     "db_host" => "localhost"
 );
